cc9b3280b6a53499aeb8f15d51f2ba89d425801d
Lentokone <44865524+Aik-10@users.noreply.github.com>
2024-03-09 17:09:53 +0200

    fix(type): Fixed env file type

--- src/Types/env.d.ts
+++ src/Types/env.d.ts
@@ -1,13 +1,13 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            API_RATELIMIT_MAX: number;
-            API_PORT: number;
+            API_RATELIMIT_MAX: string | undefined;
+            API_PORT: string | undefined;
             API_JWT_TOKEN: string;
 
             REDIS_PASSWORD: string;
             REDIS_HOST: string;
-            REDIS_PORT: number;
+            REDIS_PORT: string | undefined;
 
             MYSQL_POOL_CONNECTION: string;
         }
