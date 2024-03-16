8e0062c05292f442b0e7dc94d170c6650f254eae
Lentokone <44865524+Aik-10@users.noreply.github.com>
2024-03-16 14:50:52 +0200

    feat: db connection testing

---
+++ src/Controllers/CardController.ts
@@ -0,0 +1,16 @@
+export class CardController {
+
+    
+
+    constructor(cardId: CardId) {
+
+    }
+
+
+    public async getAccountId(): Promise<number> {
+        return 1;
+    }
+    
+    async asda() {}
+
+}
